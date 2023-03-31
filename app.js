4b8997a849eed957194cb16220cddc65fe9ceea0
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-03-31 14:32:50 -0400

    message

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
 
 app.get("/", function(req, res){
-    res.render("home", {StartingContent:homeStartingContent})
+    res.render("home", {
+        StartingContent:homeStartingContent,
+        posts: posts
+    })
 });
 
 app.get("/about", function(req, res){
@@ -32,7 +36,14 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-    console.log(req.body.postTitle);
+    const post = {
+        Title: req.body.postTitle,
+        Content: req.body.postBody
+    };
+
+    posts.push(post);
+
+    res.redirect('/');
 });
 
 
