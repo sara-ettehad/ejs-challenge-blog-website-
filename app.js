e7b6493df89e50fd8ee01ec2d0aa7559f3eea27f
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-03-31 13:29:29 -0400

    message

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
+
+app.get("/", function(req, res){
+    res.render("home", {StartingContent:homeStartingContent})
+});
+
+app.get("/about", function(req, res){
+    res.render("about", {aboutContent:aboutContent})
+});
+
+app.get("/content", function(req, res){
+    res.render("content", {contactContent:contactContent})
+});
+
+app.get("/compose", function(req, res){
+    res.render("compose")
+});
+
+app.post("/compose", function(req, res){
+    console.log(req.body.postTitle);
+});
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
